a1313a40fdcdb96f44d8a0805b0ba661388a2994
mastersenpai05 <pampangavergil0405@gmail.com>
2022-05-03 22:53:29 +0800

    E7446: Misc Changes

---
+++ twrp_E7446.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := E7446
+PRODUCT_NAME := twrp_E7446
+PRODUCT_BRAND := micromax
+PRODUCT_MODEL := E7446
+PRODUCT_MANUFACTURER := micromax
